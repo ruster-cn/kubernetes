9a485724502ebcbfa4bec2275d9e7f809566eeac
ruster-cn <peiminmin@yeah.net>
2020-08-18 17:46:21 +0800

    20200814

--- example/client-go/rest-client/main.go
+++ example/client-go/rest-client/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main(){
-	
+import (
+	"time"
+
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+)
+
+func main() {
+	stop := make(chan struct{}, 1)
+	defer close(stop)
+	config := &rest.Config{
+		Host: "",
+	}
+	client := kubernetes.NewForConfigOrDie(config)
+	factory := informers.NewSharedInformerFactory(client, 1*time.Second)
+	nodeInformer := factory.Core().V1().Pods().Informer()
+	nodeInformer.AddEventHandler(&cache.ResourceEventHandlerFuncs{})
+	go factory.Start(stop)
+	cache.WaitForCacheSync(stop, nodeInformer.HasSynced)
+
 }
